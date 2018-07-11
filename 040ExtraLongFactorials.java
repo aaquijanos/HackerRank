d9c608fc7cabc6b732122feb179904b5e55c5cfc
aaquijanos <40473096+aaquijanos@users.noreply.github.com>
2018-07-11 16:53:47 -0500

    Solución Challenge 40 HackerRank

---
+++ 040ExtraLongFactorials.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the extraLongFactorials function below.
+    static void extraLongFactorials(int n) {
+        BigInteger factorial = BigInteger.valueOf(n);
+        for(int i = 1; i < n; i++){
+            int mult = n - i;
+            //System.out.println(n-i);
+            factorial = factorial.multiply(BigInteger.valueOf(mult));
+        }
+        System.out.println(factorial);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
+}
